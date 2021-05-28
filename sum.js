77e5e8e34cbfeeb556ef1df0ac078c58296b3dfb
ANKIT <you@example.com>
2021-05-28 15:03:22 +0530

    changed

--- sum.js
+++ sum.js
@@ -1,13 +1,5 @@
 function sum(a, b) {
     return a + b;
   }
-function sub(a,b){
-  return a - b;
-}
-function mul(a,b){
-  return a * b;
-}
-function div(a,b){
-  return a / b;
-}
-  module.exports = sum,sub,mul,div;
+
+  module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -3,12 +3,3 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
-test('substracts 2 - 1 to equal 1', () => {
-  expect(sub(2, 1)).toBe(1);
-});
-test('multiplies 1 * 2 to equal 2', () => {
-  expect(mul(1, 2)).toBe(2);
-});
-test('divides 10 / 5 to equal 2', () => {
-  expect(div(10, 5)).toBe(2);
-});
